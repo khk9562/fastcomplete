32aa9998e390aeb98cdc1faa9b7b11845abdcaa1
stella <khk9562@naver.com>
2024-06-12 15:53:47 +0900

    comment 모델 구성 및 post_id에 따른 코멘트 작성 api 구현

--- storeapi/routers/post.py
+++ storeapi/routers/post.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from storeapi.models.post import UserPost, UserPostIn
+from storeapi.models.post import Comment, CommentIn, UserPost, UserPostIn
 
 router = APIRouter()
 post_table = {}
+comment_table = {}
+
+
+def find_post(post_id: int):
+    return post_table.get(post_id)
 
 
 @router.post("/post", response_model=UserPost)
@@ -18,3 +23,16 @@ async def create_post(post: UserPostIn):
 @router.get("/post", response_model=list[UserPost])
 async def get_all_posts():
     return list(post_table.values())
+
+
+@router.post("/comment", response_model=Comment)
+async def create_comment(comment: CommentIn):
+    post = find_post(comment.post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    data = comment.dict()
+    last_record_id = len(comment_table)
+    new_comment = {**data, "id": last_record_id}
+    comment_table[last_record_id] = new_comment
+    return new_comment
