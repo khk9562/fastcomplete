a2bd89b9bd65552e486c59ab1d519e1e0596a5c2
stella <khk9562@naver.com>
2024-06-12 14:19:40 +0900

    get list api 완

--- storeapi/main.py
+++ storeapi/main.py
@@ -22,3 +22,8 @@ async def create_post(post: UserPostIn):
     new_post = {**data, "id": last_record_id}
     post_table[last_record_id] = new_post
     return new_post
+
+
+@app.get("/post", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
