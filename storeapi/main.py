0c302b6bc9e71c74744a430b411aafbde2482b76
stella <khk9562@naver.com>
2024-06-12 14:16:00 +0900

    /post api 작성 및 postman test 완료

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")  # api.com/
-async def root():
-    return {"message": "Hello World"}
+class UserPostIn(BaseModel):
+    body: str
+
+
+class UserPost(UserPostIn):
+    id: int
+
+
+post_table = {}
+
+
+@app.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.dict()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
