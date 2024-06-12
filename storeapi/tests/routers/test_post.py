0f53f9cd460b1441e0d4f8ece6924775939b70f6
stella <khk9562@naver.com>
2024-06-12 17:07:20 +0900

    body 값이 없을 경우 포스트 생성하는 api 작성

--- storeapi/tests/routers/test_post.py
+++ storeapi/tests/routers/test_post.py
@@ -10,3 +10,33 @@ async def create_post(body: str, async_client: AsyncClient) -> dict:
 @pytest.fixture()
 async def created_post(async_client: AsyncClient) -> dict:
     return await create_post("Test post", async_client)
+
+
+@pytest.mark.anyio_backend
+async def test_create_post(async_client: AsyncClient):
+    body = "Test post"
+
+    response = await async_client.post(
+        "/post",
+        json={"body": body},
+    )
+
+    assert response.status_code == 201
+    assert {"id": 0, "body": body} <= response.json().items()
+
+
+# write a test that attempts to create a post, but it doesn't pass a "body" key in the JSON payload.
+
+
+async def test_create_post_no_body(async_client: AsyncClient):
+    response = await async_client.post("/post", json={})
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            }
+        ]
+    }
