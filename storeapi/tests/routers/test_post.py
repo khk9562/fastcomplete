672ead988d0b1dc8944b3953102f1544ea23d868
stella <khk9562@naver.com>
2024-06-12 17:14:25 +0900

    모든 포스트 반환 받는 api, 댓글 생성 api 구현 및 테스트

--- storeapi/tests/routers/test_post.py
+++ storeapi/tests/routers/test_post.py
@@ -7,9 +7,21 @@ async def create_post(body: str, async_client: AsyncClient) -> dict:
     return response.json()
 
 
+async def create_comment(body: str, post_id: int, async_client: AsyncClient) -> dict:
+    response = await async_client.post(
+        "/comment", json={"body": body, "post_id": post_id}
+    )
+    return response.json()
+
+
 @pytest.fixture()
 async def created_post(async_client: AsyncClient) -> dict:
     return await create_post("Test post", async_client)
+
+
+@pytest.fixture()
+async def created_comment(async_client: AsyncClient, created_post: dict) -> dict:
+    return await created_comment("Test Comment", created_post["id"], async_client)
 
 
 @pytest.mark.anyio_backend
@@ -40,3 +52,31 @@ async def test_create_post_no_body(async_client: AsyncClient):
             }
         ]
     }
+
+
+@pytest.mark.anyio_backend
+async def test_get_all_posts(async_client: AsyncClient, created_post: dict):
+    response = await async_client.get("/post")
+
+    assert response.status_code == 200
+    assert response.json == [created_post]
+
+
+@pytest.mark.anyio_backend
+async def test_create_comment(async_client: AsyncClient, created_post: dict):
+    body = "Test comment"
+    post_id = created_post["id"]
+
+    response = await async_client.post(
+        "/comment",
+        json={"body": body, "post_id": post_id},
+    )
+
+    assert response.status_code == 201
+    assert {
+        "id": 0,
+        "body": body,
+        "post_id": post_id,
+    }.items() <= response.json().items()
+
+    # items() 메서드는 딕셔너리의 키-값 쌍을 튜플 형태로 반환합니다.
