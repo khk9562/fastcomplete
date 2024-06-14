80145e6c12cfc3cac44538f9de08f66e3b71c26f
stella <khk9562@naver.com>
2024-06-14 14:00:18 +0900

    api 별 로거 추가

--- storeapi/routers/post.py
+++ storeapi/routers/post.py
@@ -26,8 +26,10 @@ async def find_post(post_id: int):
 
 @router.post("/post", response_model=UserPost, tags=["posts"])
 async def create_post(post: UserPostIn):
+    logger.info("Creating post")
     data = post.dict()
     query = post_table.insert().values(data)
+    logger.debug(query)
     last_record_id = await database.execute(query)
     await database.execute(query)
     return {**data, "id": last_record_id}
@@ -40,8 +42,8 @@ async def create_post(post: UserPostIn):
 @router.get("/post", response_model=list[UserPost], tags=["posts"])
 async def get_all_posts():
     # return list(post_table.values())
-
     logger.info("Getting all posts")
+
     query = post_table.select()
 
     logger.debug(query)
@@ -51,12 +53,17 @@ async def get_all_posts():
 
 @router.post("/comment", response_model=Comment, status_code=201, tags=["posts"])
 async def create_comment(comment: CommentIn):
+    logger.info("Creating comment")
+
     post = await find_post(comment.post_id)
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
 
     data = comment.dict()
     query = comment_table.insert().values(data)
+
+    logger.debug(query)
+
     last_record_id = await database.execute(query)
     return {**data, "id": last_record_id}
     # **data에서 **는 for desctructuring a dictionary into another dicctionary
@@ -69,7 +76,9 @@ async def create_comment(comment: CommentIn):
 
 @router.get("/post/{post_id}/comment", response_model=list[Comment], tags=["posts"])
 async def get_comments_on_post(post_id: int):
+    logger.info("Getting comments on post")
     query = comment_table.select().where(comment_table.c.post_id == post_id)
+    logger.debug(query)
     return await database.fetch_all(query)  # return_value["body"]
     # return [
     #     comment for comment in comment_table.values() if comment["post_id"] == post_id
@@ -78,6 +87,7 @@ async def get_comments_on_post(post_id: int):
 
 @router.get("/post/{post_id}", response_model=UserPostWithComments, tags=["posts"])
 async def get_post_with_comments(post_id: int):
+    logger.info("Getting post and its comments")
     post = await find_post(post_id)
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
